f31572c0dece9a625374e783f4def7ee6170617a
Christopher Wee <chriswee168@gmail.com>
2026-04-21 10:57:49 +0800

    Create component for calculator buttons
    Buttons are used to append and modify the main equation string.

---
+++ src/CalcButton.jsx
@@ -0,0 +1,18 @@
+import { useRef, useState } from 'react';
+/**
+ * Component for calculator button.
+ * 
+ * @param {string} calcLabel Label for this button.
+ * @param {number} posY Button Y position.
+ * @param {number} posX Button X position.
+ * @param {string} equationState String containing equation.
+ * @param {Dispatch<SetStateAction<string>} updateEquation Function to update equation string.
+ * @returns 
+ */
+export function CalcButton({calcLabel, posY, posX, equationState, updateEquation})
+{
+    const buttonLabel = useRef(calcLabel);
+    return (
+        <button type="button" style={{top: posY, left: posX}}>{buttonLabel}</button>
+    )
+}

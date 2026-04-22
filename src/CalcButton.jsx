84eb0b4feb4a0c50cb06993b3bd0c5f4d03f2503
Christopher Wee <chriswee168@gmail.com>
2026-04-22 09:44:39 +0800

    Edit JSDoc for CalcButton
    Ensure types are recognizable by VS Code intellisense.

--- src/CalcButton.jsx
+++ src/CalcButton.jsx
@@ -3,11 +3,12 @@ import styles from "./CalcButton.module.css";
 /**
  * Component for calculator button.
  * 
- * @param {string} calcLabel Label for this button.
- * @param {number} posY Button Y position.
- * @param {number} posX Button X position.
- * @param {string} equationState String containing equation.
- * @param {Dispatch<SetStateAction<string>} updateEquation Function to update equation string.
+ * @param {Object} props
+ * @param {string} props.calcLabel Label for this button.
+ * @param {number} props.posY Button Y position.
+ * @param {number} props.posX Button X position.
+ * @param {string} props.equationState String containing equation.
+ * @param {Dispatch<SetStateAction<string>} props.updateEquation Function to update equation string.
  * @returns 
  */
 export function CalcButton({calcLabel, posY, posX, equationState, updateEquation})

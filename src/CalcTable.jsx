e4646a804d5fc6ec589954ce1d2774f3fb143b9c
Christopher Wee <chriswee168@gmail.com>
2026-04-23 21:41:13 +0800

    Create CalcTable component
    Groups calculator buttons in a table.

---
+++ src/CalcTable.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { CalcButton } from './CalcButton'
+import styles from './CalcTable.module.css'
+
+/**
+ * Component for table containing calculator buttons.
+ * 
+ * @returns CalcTable component.
+ */
+export default function CalcTable() {
+  return (
+    <>
+      <table className={styles.calcTable}>
+        <tbody>
+          <tr>
+            <td><CalcButton calcLabel="1" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="2" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="3" borderRadii={[0, 0, 0, 0]}/></td>
+          </tr>
+          <tr>
+            <td><CalcButton calcLabel="4" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="5" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="6" borderRadii={[0, 0, 0, 0]}/></td>
+          </tr>
+          <tr>
+            <td><CalcButton calcLabel="7" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="8" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="9" borderRadii={[0, 0, 0, 0]}/></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}

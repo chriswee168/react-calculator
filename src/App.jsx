8ac143544a3cfa98bb034df4bf3824ca1a091ac8
Christopher Wee <chriswee168@gmail.com>
2026-04-24 22:44:14 +0800

    Create string state for equation.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { CalcButton } from './CalcButton'
 import CalcTable from './CalcTable'
 
 export default function App() {
+
+  // Main equation string.
+  const [equation, updateEquation] = useState("");
+
   return (
     <>
       <CalcTable />

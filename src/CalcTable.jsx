4ca91c7ab309d5682a150ee823183e73fe3ac95b
Christopher Wee <chriswee168@gmail.com>
2026-04-25 23:39:15 +0800

    Add border radius for corner calculator buttons

--- src/CalcTable.jsx
+++ src/CalcTable.jsx
@@ -22,28 +22,28 @@ export default function CalcTable({equationState, updateEquation}) {
       <table className={styles.calcTable}>
         <tbody>
           <tr>
-            <td><CalcButton calcLabel="1" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="2" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="3" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="+" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="1" borderRadii={[20, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="2"/></td>
+            <td><CalcButton calcLabel="3"/></td>
+            <td><CalcButton calcLabel="+" borderRadii={[0, 20, 0, 0]}/></td>
           </tr>
           <tr>
-            <td><CalcButton calcLabel="4" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="5" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="6" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="-" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="4"/></td>
+            <td><CalcButton calcLabel="5"/></td>
+            <td><CalcButton calcLabel="6"/></td>
+            <td><CalcButton calcLabel="-"/></td>
           </tr>
           <tr>
-            <td><CalcButton calcLabel="7" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="8" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="9" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="*" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="7"/></td>
+            <td><CalcButton calcLabel="8"/></td>
+            <td><CalcButton calcLabel="9"/></td>
+            <td><CalcButton calcLabel="*"/></td>
           </tr>
           <tr>
-            <td><CalcButton calcLabel="(" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="0" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel=")" borderRadii={[0, 0, 0, 0]}/></td>
-            <td><CalcButton calcLabel="/" borderRadii={[0, 0, 0, 0]}/></td>
+            <td><CalcButton calcLabel="(" borderRadii={[0, 0, 20, 0]}/></td>
+            <td><CalcButton calcLabel="0"/></td>
+            <td><CalcButton calcLabel=")"/></td>
+            <td><CalcButton calcLabel="/" borderRadii={[0, 0, 0, 20]}/></td>
           </tr>
         </tbody>
       </table>

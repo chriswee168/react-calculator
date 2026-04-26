e07946568dfd7dabd907276444b38d9b47cdfff2
Christopher Wee <chriswee168@gmail.com>
2026-04-26 18:42:31 +0800

    Add .outer class

--- src/EquationWindow.jsx
+++ src/EquationWindow.jsx
@@ -14,6 +14,7 @@ export function EquationWindow({
 {
     return (
         <div
+            className={styles.outer}
             style={{
                 borderTopLeftRadius: borderRadii[0],
                 borderTopRightRadius: borderRadii[1],

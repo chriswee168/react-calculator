5655daffe8567c93ce3f809f9583c9048dde5363
Christopher Wee <chriswee168@gmail.com>
2026-05-02 13:33:05 +0800

    Loop through each character in the equation string

--- src/calc-funcs/calculate-eq.js
+++ src/calc-funcs/calculate-eq.js
@@ -7,4 +7,17 @@ export function calculate(eqString)
 {
     // Mutable list to store characters from eqString.
     const charStack = [];
+
+    // For each character in equation.
+    for (let i = 0; i < eqString.length; i++)
+    {
+        if (eqString[i] == ')')
+        {
+            
+        }
+        else
+        {
+            charStack.push(eqString[i])
+        }
+    }
 }
